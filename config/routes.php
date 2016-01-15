c3faa2932a54db4aafa5f5f323cf247c4e2281c0
Jack Polgar <jack@polgar.id.au>
2016-01-16 00:41:28 +1100

    Start admin dashboard.

--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,9 @@ return [
     '/login' => 'routes/users/login.php',
     '/register' => 'routes/users/register.php',
 
+    // -------------------------------------------------------------------------
+    // Admin
+    '/admin' => 'routes/admin/dashboard.php',
     '/{pslug}' => 'routes/project.php',
     '/{pslug}/tickets' => 'routes/tickets.php',
     '/{pslug}/tickets/{id}' => 'routes/tickets/show.php'
