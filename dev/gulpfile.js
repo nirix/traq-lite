bfb4956be41889478950dd8166e5c9e2093fc540
Jack Polgar <jack@polgar.id.au>
2016-01-16 00:32:46 +1100

    Use a highly customised bootstrap.

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -3,41 +3,44 @@ var gulp       = require('gulp')
     sass       = require('gulp-sass'),
     coffee     = require('gulp-coffee'),
     concat     = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps')
-    path       = require('path');
+    uglify     = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps');
 
 var beSassy = function() {
     console.log('Being Sassy');
 
-    gulp.src(['./scss/traq.scss'])
-    .pipe(sass().on('error', sass.logError))
+    gulp.src([
+        './scss/common.scss',
+        './scss/traq.scss',
+        './scss/admin.scss'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed', includePaths: 'node_modules/bootstrap-sass/assets/stylesheets'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('../assets/css'));
 }
 
 var makeCoffee = function() {
-    gulp.src('./coffee/*.coffee')
+    console.log('Making coffee');
+
+    gulp.src('coffee/*.coffee')
         .pipe(coffee())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('../assets/js'));
 }
 
 gulp.task('compile', function(){
-    gulp.src(['./scss/traq.scss'])
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('../assets/css'));
-
+    beSassy();
     makeCoffee();
+    gulp.start('assets');
 });
 
 // Watch for changes
 gulp.task('watch', function(){
-    watch('scss/*.scss', beSassy);
-    watch('scss/*/*.scss', beSassy);
-    watch('draper/scss/*.scss', beSassy);
-    watch('draper/scss/*/*.scss', beSassy);
-
-    //watch('./coffee/*.coffee', makeCoffee);
+    watch('scss/**/*.scss', beSassy);
+    watch('coffee/**/*.coffee', makeCoffee);
 });
 
 // Compile Sass
@@ -45,3 +48,19 @@ gulp.task('sass', beSassy);
 
 // Compile CoffeeScript
 gulp.task('coffee', makeCoffee);
+
+// JavaScripts
+gulp.task('assets', function() {
+    gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('js.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('../assets/js'));
+
+    gulp.src(['node_modules/font-awesome/fonts/*'])
+        .pipe(gulp.dest('../assets/fonts'));
+});
